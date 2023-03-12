b5987499f624e44bea0971428a308a1e1a7e1dd7
Tejas Shah <61719799+Tshah-95@users.noreply.github.com>
2023-03-11 22:51:58 -0500

    Update external.d.ts

--- external.d.ts
+++ external.d.ts
@@ -2,3 +2,5 @@ declare module '@next/font/local';
 declare module '@next/font/google';
 declare module 'classnames';
 declare module '@vercel/analytics/react';
+declare module 'framer-motion';
+declare module 'react-wrap-balancer';
