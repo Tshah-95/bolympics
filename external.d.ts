916fdfed4632d788409262ff84b16ca03b4355d6
Tejas Shah <61719799+Tshah-95@users.noreply.github.com>
2024-03-21 00:10:46 -0400

    nuke

--- external.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@next/font/local';
-declare module '@next/font/google';
-declare module 'classnames';
-declare module '@vercel/analytics/react';
-declare module 'framer-motion';
-declare module 'react-wrap-balancer';
-declare module '@radix-ui/react-popover';
-declare module '@prisma/client';
