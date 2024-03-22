2b123a75e59adea80e06a84716c5bead3c33af77
Tejas Shah <61719799+Tshah-95@users.noreply.github.com>
2024-03-22 01:57:34 -0400

    title and nav w/o auth

---
+++ app/components/navbar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import useScroll from "@/lib/hooks/use-scroll";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NavBar() {
+  const scrolled = useScroll(50);
+
+  return (
+    <>
+      <div
+        className={`fixed top-0 w-full flex justify-center ${
+          scrolled ? " bg-slate-700/30 backdrop-blur-xl" : "bg-slate-600/0"
+        } z-30 transition-all`}
+      >
+        <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-center w-full">
+          <Link
+            href="/"
+            className="flex items-center font-display text-2xl text-white"
+          >
+            <Image
+              src="/bolympics-title.png"
+              alt="bolympics logo"
+              width="150"
+              height="30"
+            ></Image>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
